ad432faaba166ae73d3943803ea5898642a7791f
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-02-15 13:31:52 -0300

    01 - Result Class

---
+++ test/result_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_mvvm_udemy/core/result/result.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Should test Ok Result', () {
+    test('Should create a Ok Result', () {
+      final result = Result.ok("Ok");
+
+      expect((result as Ok).value, 'Ok');
+    });
+
+    test("Should create a Error Result", () {
+      final result = Result.error(Exception('Ocorreu um erro...'));
+
+      expect((result as Error).value, isA<Exception>());
+    });
+
+    test('Should create a Ok Result with Extension', () {
+      final result = "Ok".ok();
+
+      expect(result.asOk.value, 'Ok');
+    });
+
+    test('Should create a Error Result with Extension', () {
+      final result = Exception('Ocorreu um erro...').error();
+
+      expect(result.asError.value, isA<Exception>());
+    });
+  });
+}
