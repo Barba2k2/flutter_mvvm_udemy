87b3f6df6d82f4617a753dbe9cf4b7d1bfee322b
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-02-16 00:44:41 -0300

    Services Part 2

--- lib/data/services/api/api_client.dart
+++ lib/data/services/api/api_client.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 
-import '../../domain/modes/todo.dart';
-import '../../utils/result/result.dart';
+import '../../../domain/modes/todo.dart';
+import '../../../utils/result/result.dart';
 
 class ApiClient {
   final String _host;
@@ -42,6 +42,40 @@ class ApiClient {
       return Result.error(e);
     } finally {
       client.close();
+    }
+  }
+
+  Future<Result<Todo>> postTodo(Todo todo) async {
+    final client = _clientHttpFactory();
+
+    try {
+      final request = await client.post(_host, _port, '/todos');
+
+      request.write(
+        jsonEncode(
+          {
+            'name': todo.name,
+          },
+        ),
+      );
+
+      final response = await request.close();
+
+      if (response.statusCode == 201) {
+        final stringData = await response.transform(utf8.decoder).join();
+
+        final json = jsonDecode(stringData) as Map<String, dynamic>;
+
+        final createdTodo = Todo.fromJson(json);
+
+        return Result.ok(createdTodo);
+      } else {
+        return Result.error(
+          HttpException('Invalid response'),
+        );
+      }
+    } on Exception catch (e) {
+      return Result.error(e);
     }
   }
 }
--- test/data/services/api_client_test.dart
+++ test/data/services/api_client_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_mvvm_udemy/data/services/api_client.dart';
+import 'package:flutter_mvvm_udemy/data/services/api/api_client.dart';
 import 'package:flutter_mvvm_udemy/domain/modes/todo.dart';
 import 'package:flutter_mvvm_udemy/utils/result/result.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -15,6 +15,16 @@ void main() {
       final result = await apiClient.getTodos();
 
       expect(result.asOk.value, isA<List<Todo>>());
+    });
+
+    test("Should return a Todo when creating postTodo()", () async {
+      // final randomNumber = Random(1200).toString();
+
+      final Todo todoToCreate = Todo(name: 'Todo created on Test');
+
+      final result = await apiClient.postTodo(todoToCreate);
+
+      expect(result.asOk.value, isA<Todo>());
     });
   });
 }
